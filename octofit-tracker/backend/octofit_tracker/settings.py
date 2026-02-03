98632e6a3f8716ea9c6f99fbd06d6264086a76c3
amlan-pattnayak <amlan.g.pattnayak@gmail.com>
2026-02-03 05:53:47 +0000

    Django bakend configured for Codespace URL and localhost

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-sgj2w!bj$0lgu#*znzwj1e+2^08^ks-z11xteo!&ol(+p1r^*y
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow codespace and localhost hosts
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
